0fc1c7908e61ac28257633d9ccc8debc785b738b
asukas00 <jessere2025@gmail.com>
2025-09-12 07:31:28 -0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,25 @@
+""" x = 3
+y = float(3)
+print(x,y)
+ """
+""" values = [1,2,23,5,7,2,30,15]
+print(values)
+for i in values:
+    print(i)
+
+print(values[0])
+print(values[3])
+ """
+"test"
+["t","e","s" "t"]
+
+x = "This is a thing"
+y = x.split()
+z = y[3]
+print(y)
+print(z)
+
+#first write a input that says anything for me im putting what are you doing
+#craft the function that the user can write
+#how much words are in the string
+y = input("how many words are in this sentence")
